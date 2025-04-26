dfac6dfdd31c41e1985acbbcc978e9fa63318b1a
RabHanz <53461530+RabHanz@users.noreply.github.com>
2025-04-27 01:28:49 +0500

    Add files via upload

---
+++ jtbd_odoo_crm/models/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# JTBD Specific Models
+from . import jtbd_job_pattern
+from . import jtbd_force_analysis
+from . import jtbd_force_item
+from . import jtbd_outcome_pattern
+from . import jtbd_outcome_mapping
+from . import jtbd_additional_outcome
+from . import jtbd_outcome_milestone
+from . import jtbd_integration_settings
+from . import jtbd_common_force_description
+from . import jtbd_intent_signal              # Added in P3
+from . import jtbd_job_evolution              # Added in P3
+from . import jtbd_content_alignment          # Added in P3
+from . import jtbd_job_portfolio              # Added in P3
+from . import jtbd_knowledge_transfer         # Added in P3
+from . import jtbd_integration_field_mapping  # Added in P3
+from . import jtbd_integration_log            # Added in P3
+from . import jtbd_trace_link                 # Added in P3
+from . import jtbd_settings                   # Added in P3
+from . import jtbd_analytics_report           # Added in P3
+
+# Inherited Models
+from . import crm_lead
+from . import product_template
+from . import sale_order
+from . import project_project
+from . import project_task
