83ccecdf1713780bc8a0a495fd3ac8dc97b1a12c
RabHanz <53461530+RabHanz@users.noreply.github.com>
2025-04-28 18:07:07 +0500

    Delete jtbd_odoo_crm directory

--- jtbd_odoo_crm/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import jtbd_job_statement_builder
-from . import jtbd_common_force_wizard
-from . import jtbd_outcome_template_wizard
-from . import jtbd_outcome_suggestion_wizard
-from . import jtbd_create_trace_link_wizard
